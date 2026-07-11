4db82cc1e1e94856a0d363822b38c97a552ac787
Sri Venkateswaran <61236812+srivenkateswaran6002@users.noreply.github.com>
2026-07-11 10:15:20 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        std::unordered_map<char , int>mS , mT;
+        int n = s.length();
+        for (int i = 0 ; i < n ; i++) {
+            mS[s[i]]++;
+            mT[t[i]]++;
+        }
+        for (char c : s) if (mS[c] != mT[c]) return false;
+        return true;
+    }
+};
