9ee721d3cb3c516aa6b280555d4eb674996c7856
Sri Venkateswaran <61236812+srivenkateswaran6002@users.noreply.github.com>
2026-09-17 11:49:31 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin() , intervals.end() , [](Interval &a , Interval& b){return a.start < b.start;});
+        for (auto& i : intervals) cout << i.start << " " << i.end << endl;
+        for (int i = 1 ; i < intervals.size() ; i++){
+            if (intervals[i].start < intervals[i - 1].end) return false;
+        }
+        return true;
+    }
+};
